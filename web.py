24614fc500392d2b7e6b8c94b53c90ab59ba7cc8
Mishrilal Chhaparia <mishrilalchhaparia@gmail.com>
2021-10-09 02:29:08 +0530

    UI - uploading files in filesystem

--- web.py
+++ web.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+import os.path
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -8,9 +13,13 @@ def hello_world():
     return render_template('index.html')
 
 
-def move_forward():
-    # Moving forward code
-    print("Moving Forward...")
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        systemPath = '/Users/mishrilalchhaparia/Projects/ImgEnh'    # Change this path - Hosting
+        f.save(systemPath + app.config['UPLOAD_FOLDER'] + '/' + secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
