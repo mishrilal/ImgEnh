6a4f53beccf5ba3638e51581574750676484e33b
Mishrilal Chhaparia <mishrilalchhaparia@gmail.com>
2021-10-24 13:21:45 +0530

    History

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os.path
 from datetime import datetime
+
+import shortuuid
 from flask import Flask, render_template, request, send_file, jsonify
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
@@ -10,7 +12,6 @@ import he
 import eff
 
 app = Flask(__name__)
-
 
 UPLOAD_FOLDER = 'static/uploads'
 fileAnalyse = ""
@@ -37,20 +38,29 @@ def allowed_file(filename):
 
 @app.route("/", methods=["POST", "GET"])
 def hello_world():
-    # if request.method == "POST":
-    #     f = request.files['file']
-    #     global filenameG
-    #     filenameG = secure_filename(f.filename)
-    #     systemPath = os.getcwd()  # OS Dir Path - Change this for Hosting
-    #     f.save(systemPath + app.config['UPLOAD_FOLDER'] + '/' + filenameG)
-    # files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('index.html')
+
+
+@app.route("/history", methods=["POST", "GET"])
+def history():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT * FROM records")
+    records = cur.fetchall()
+    mysql.connection.commit()
+    cur.close()
+    print(records)
+    return render_template('records.html', records=records)
 
 
 @app.route('/dhe')
 def analyseDHE():
     global fileDownload
     fileDownload = dhe.analyse(fileAnalyse)
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("UPDATE records SET dhe=%s WHERE upload=%s",
+                (fileDownload, fileAnalyse))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'htmlresponse': render_template('responseOutput.html', filenameImage=fileDownload)})
 
 
@@ -58,6 +68,11 @@ def analyseDHE():
 def analyseHE():
     global fileDownload
     fileDownload = he.analyse(fileAnalyse)
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("UPDATE records SET he=%s WHERE upload=%s",
+                (fileDownload, fileAnalyse))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'htmlresponse': render_template('responseOutput.html', filenameImage=fileDownload)})
 
 
@@ -65,6 +80,11 @@ def analyseHE():
 def analyseEFF():
     global fileDownload
     fileDownload = eff.analyse(fileAnalyse)
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("UPDATE records SET eff=%s WHERE upload=%s",
+                (fileDownload, fileAnalyse))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'htmlresponse': render_template('responseOutput.html', filenameImage=fileDownload)})
 
 
@@ -76,29 +96,28 @@ def download():
 
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
+    newName = shortuuid.uuid()
     file = request.files['uploadFile']
     filename = secure_filename(file.filename)
+    fileExt = filename.rsplit('.', 1)[1]
+    filename = newName + '.' + fileExt
     global fileAnalyse, filenameImage
     fileAnalyse = filename
     if file and allowed_file(file.filename):
         systemPath = os.getcwd()
         file.save(systemPath + '/' + app.config['UPLOAD_FOLDER'] + '/' + filename)
-        filenameImage = file.filename
 
         today = datetime.today()
-        # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        # cur.execute("INSERT INTO uploads (file_name, upload_time) VALUES (%s,%s)", [filenameImage, today])
-        # conn.commit()
-        # cur.close()
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cur.execute("INSERT INTO records(uploadTime, upload) VALUES (%s, %s)",
                     (today, filename))
         mysql.connection.commit()
         cur.close()
+
         msg = 'File successfully uploaded ' + file.filename + ' to the database!'
     else:
         msg = 'Invalid Upload only png, jpg, jpeg, gif'
-    return jsonify({'htmlresponse': render_template('response.html', msg=msg, filenameImage=filenameImage)})
+    return jsonify({'htmlresponse': render_template('response.html', msg=msg, filenameImage=filename)})
 
 
 if __name__ == '__main__':
